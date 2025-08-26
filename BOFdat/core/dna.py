7151c3d23393317f9a525bb5124530a8f4d337f8
Jeroen Gilis <gilis@chalmers.se>
2025-08-26 16:18:40 +0200

    update dna.py

--- BOFdat/core/dna.py
+++ BOFdat/core/dna.py
@@ -6,6 +6,7 @@ This module generates BOFsc for the 4 bases of DNA (dATP, dTTP, dCTP and dGTP)
 
 """
 import warnings
+from collections import Counter
 
 BASES = ['A', 'T', 'C', 'G']
 
@@ -32,78 +33,89 @@ def _import_model(path_to_model):
     else:
         raise Exception('Model format not compatible, provide xml or json')
 
-def _get_number_of_bases(genome):
-    # Counts the number of each letter in the genome
-    base_genome = {'A': 0, 'T': 0, 'C': 0, 'G': 0}
+def _get_number_of_bases(genome, ignoreLower=False):
+    total_counts = Counter()
     for record in genome:
-        for element in record:
-            value = base_genome.get(element.upper())
-            if value == None:
-                continue
-            else:
-                new_value = value + 1
-                base_genome[element] = new_value
+        total_counts.update(str(record.seq))
+    
+    if ignoreLower:
+        for base in ('a', 't', 'c', 'g'):
+            total_counts.pop(base, None)
+    else:
+        for base_upper, base_lower in [('A', 'a'), ('T', 't'), ('C', 'c'), ('G', 'g')]:
+            total_counts[base_upper] += total_counts.pop(base_lower, 0)
 
-    return base_genome
+    return {base: total_counts.get(base, 0) for base in ['A', 'T', 'C', 'G']}
 
 def _get_ratio(base_genome):
     # Get the ratios for each letter in the genome
-    ratio_genome = {'A': 0, 'T': 0, 'C': 0, 'G': 0}
+    # TODO running the original _get_ratio leads to ratios that sum to 0.5, not 1
+    # While this may be intentional, it is very strange.
+    # Below, I adapted the code to what I think it should be.
+    # This will be equivalent to running the original code and 
+    # multiplying the ratios, or the downstream coefficients, by 2
+    total = 2*sum(base_genome.values()) # double-stranded
+    at_sum = base_genome['A'] + base_genome['T']
+    gc_sum = base_genome['G'] + base_genome['C']
+    return {k: at_sum / total if k in 'AT' else gc_sum / total for k in 'ATGC'}
 
-    #DNA is double strand so the number of A = number of T and C = G
-    at_number = base_genome.get('A') + base_genome.get('T')
-    base_genome['A'] = at_number
-    base_genome['T'] = at_number
-    gc_number = base_genome.get('C') + base_genome.get('G')
-    base_genome['G'] = gc_number
-    base_genome['C'] = gc_number
+def _convert_to_coefficient(model, ratio_genome, DNA_RATIO, base_to_met=None):
+    
+    DIPHOSPHATE_WEIGHT = 174.951262
 
-    for letter in BASES:
-        number_of_base = float(base_genome.get(letter))
-        total = 2*(sum(base_genome.values()))
-        ratio = number_of_base / total
-        ratio_genome[letter] = ratio
+    # Dryweight of an e. coli cell in femtogram
+    # Cancels out in these equations, but makes variables interpretable
+    CELL_WEIGHT = 280
 
-    return ratio_genome
-
-def _convert_to_coefficient(model, ratio_genome, CELL_WEIGHT, DNA_RATIO):
     # Transform the ratios into mmol/gDW
     DNA_WEIGHT = CELL_WEIGHT * DNA_RATIO
 
-    DIPHOSPHATE_WEIGHT = 174.951262
+    if base_to_met is None:
+        base_to_met = {'A': model.metabolites.datp_c, 'T': model.metabolites.dttp_c,
+                       'C': model.metabolites.dctp_c, 'G': model.metabolites.dgtp_c}
 
-    base_to_bigg = {'A': model.metabolites.datp_c, 'T': model.metabolites.dttp_c,
-                    'C': model.metabolites.dctp_c, 'G': model.metabolites.dgtp_c}
     coefficients,metabolites = [],[]
 
     # Calculate the biomass coefficient for each metabolite
-    for letter in BASES:
+    for letter in step1.dna.BASES:
         ratio = ratio_genome.get(letter)
         total_weight = ratio * DNA_WEIGHT
-        metab = base_to_bigg.get(letter)
+        metab = base_to_met.get(letter)
         mol_weight = metab.formula_weight - DIPHOSPHATE_WEIGHT
         mmols_per_cell = (total_weight / mol_weight) * 1000
         mmols_per_gDW = mmols_per_cell / CELL_WEIGHT
         coefficients.append(mmols_per_gDW)
-        metabolites.append(base_to_bigg.get(letter))
+        metabolites.append(base_to_met.get(letter))
 
     DNA_coefficients = dict(zip(metabolites,[-i for i in coefficients]))
     return DNA_coefficients
 
-def generate_coefficients(path_to_fasta, path_to_model , DNA_WEIGHT_FRACTION=0.031):
+def generate_coefficients(path_to_fasta,
+                          path_to_model ,
+                          DNA_WEIGHT_FRACTION=0.031,
+                          base_to_met = None,
+                          ppi = None):
     """
     Generates a dictionary of metabolite:coefficients for the 4 DNA bases from the organism's
     DNA fasta file and the weight percentage of DNA in the cell.
 
-    :param path_to_fasta: a path to the DNA fasta file of the organism, format should be compatible with BioPython SeqIO
+    :param path_to_fasta: a path to the DNA fasta file of the organism, format should be compatible 
+    with BioPython SeqIO
 
     :param path_to_model: a path to the model, format supported are json and xml
 
     :param DNA_RATIO: the ratio of DNA in the entire cell
 
+    :param base_to_met: a dictionary with keys for each nucleotide (ACGT) and 
+    values the corresponding metabolite identifiers in the model. 
+    Default = None, in case which BIGG nomenclature is assumed.
+
+    :param ppi: a string indicating the metabolite identifier for pyrophosphate
+    (ppi) in the model.
+    Default = None, in case which BIGG nomenclature is assumed.
+
     :return: a dictionary of metabolites and coefficients
     """
-    CELL_WEIGHT = 280
     if DNA_WEIGHT_FRACTION > 1.:
         raise Exception('WEIGHT FRACTION should be a number between 0 and 1')
     #Operations
@@ -111,11 +123,16 @@ def generate_coefficients(path_to_fasta, path_to_model , DNA_WEIGHT_FRACTION=0.0
     base_in_genome = _get_number_of_bases(genome)
     ratio_in_genome = _get_ratio(base_in_genome)
     model = _import_model(path_to_model)
-    biomass_coefficients = _convert_to_coefficient(model, ratio_in_genome, CELL_WEIGHT,
-                                                  DNA_WEIGHT_FRACTION)
-    #Add Pyrophosphate synthesis as the sum of the coefficients
+    biomass_coefficients = _convert_to_coefficient(model,
+                                                   ratio_in_genome,
+                                                   DNA_WEIGHT_FRACTION, 
+                                                   base_to_met)
+    # Add pyrophosphate synthesis as the sum of the coefficients
     ppi_coeff = sum(biomass_coefficients.values())
-    ppi_dict = {model.metabolites.get_by_id('ppi_c'):-ppi_coeff}
+    if ppi is None:
+        ppi_dict = {model.metabolites.get_by_id('ppi_c'):-ppi_coeff}
+    else:
+        ppi_dict = {model.metabolites.get_by_id(ppi):-ppi_coeff}
     biomass_coefficients.update(ppi_dict)
 
     return biomass_coefficients
